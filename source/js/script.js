5b147a23e58419e2f3e71d5734b31e51ef92ee1f
ParshutinD <daniiparshutin1@gmail.com>
2022-02-01 19:04:15 +0300

    1

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+// slider
+
 const slider_info = new Swiper(".info-block__slider", {
   centeredSlides: true,
   autoplay: {
@@ -45,7 +47,7 @@ const feedback_slider = new Swiper(".feedback__slider", {
 
 const partners_slider = new Swiper(".partners__slider", {
   slidesPerView: 3,
-  spaceBetween: 22,
+  spaceBetween: 14,
   slidesPerGroup: 3,
   loop: true,
   loopFillGroupWithBlank: true,
@@ -53,4 +55,55 @@ const partners_slider = new Swiper(".partners__slider", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+  breakpoints: {
+    360: {
+      slidesPerView: 1,
+      spaceBetween: 8,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 14,
+    },
+  },
+});
+
+const swiper = new Swiper(".delivery-block-mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+const swiper2 = new Swiper(".delivery-block-mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+  autoplay: {
+    delay: 2500,
+  },
+});
+
+const advantage_slider = new Swiper(".advantage-slider", {
+  direction: "horizontal",
+});
+
+// burger-menu
+
+const open_button_menu = document.querySelector(".page-header__button-menu");
+const page_menu = document.querySelector(".page-header__menu");
+const closed_button_menu = document.querySelector(
+  ".page-header__button-menu--closed"
+);
+
+open_button_menu.addEventListener("click", function () {
+  page_menu.classList.add("page-header__menu--open");
+});
+
+closed_button_menu.addEventListener("click", function () {
+  page_menu.classList.remove("page-header__menu--open");
 });
